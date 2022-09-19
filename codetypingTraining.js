6fec2528d1671711863fa86755161ea0d970bccb
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-09-19 15:10:13 +0300

    another-one lesson

--- codetypingTraining.js
+++ codetypingTraining.js
@@ -481,22 +481,44 @@ const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 // }
 
 //22)
-function binarySearch(array, item){
-    let start     = 0,
-        end       = array.length,
-        middle    = 0,
-        isFound   = false;
+// function binarySearch(array, item){
+//     let start     = 0,
+//         end       = array.length,
+//         middle    = 0,
+//         isFound   = false;
 
+//     while(!isFound && start <= end){
+//         middle = Math.floor((start + end) / 2)
+
+//         if(array[middle] === item){
+//             isFound = true
+//             return middle
+//         }
+
+//         if(array[middle] > item){
+//             end = middle -1
+//         }else{
+//             start = middle + 1;
+//         }
+//     }
+// }
+//23)
+function binarySearch(array, item){
+    let start   = 0,
+        end     = array.length,
+        middle  = 0,
+        isFound = false;
+    
     while(!isFound && start <= end){
         middle = Math.floor((start + end) / 2)
 
         if(array[middle] === item){
-            isFound = true
-            return middle
+            isFound = true;
+            return middle;
         }
 
         if(array[middle] > item){
-            end = middle -1
+            end = middle - 1;
         }else{
             start = middle + 1;
         }
@@ -770,11 +792,25 @@ function binarySearch(array, item){
 //     }
 //}
 //19)
+// function recursiveBinarySearch(array, item, start, end){
+//     const middle = Math.floor((start + end) / 2)
+
+//     if(array[middle] === item){
+//         return middle
+//     }
+
+//     if(array[middle] > item){
+//         return recursiveBinarySearch(array, item, start, middle - 1)
+//     }else{
+//         return recursiveBinarySearch(array, item, middle + 1, end)
+//     }
+// }
+//20)
 function recursiveBinarySearch(array, item, start, end){
     const middle = Math.floor((start + end) / 2)
 
     if(array[middle] === item){
-        return middle
+        return middle;
     }
 
     if(array[middle] > item){
@@ -1151,22 +1187,39 @@ const arrForSort = [0,3,2,5,6,8,1,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32]
 //     return array;
 // }
 //20)
+// function selectionSort(array){
+//     for(let i = 0; i < array.length; i++){
+//         let indexMin = i;
+
+//         for(let j = i + 1; j < array.length; j++){
+//             if(array[j] < array[indexMin]){
+//                 indexMin = j;
+//             }
+//         }
+
+//         let tmp = array[indexMin]
+//         array[indexMin] = array[i]
+//         array[i] = tmp
+//     }
+
+//     return array
+// }
+//21)
 function selectionSort(array){
     for(let i = 0; i < array.length; i++){
         let indexMin = i;
-
         for(let j = i + 1; j < array.length; j++){
             if(array[j] < array[indexMin]){
-                indexMin = j;
+                indexMin = j
             }
         }
 
-        let tmp = array[indexMin]
-        array[indexMin] = array[i]
+        let tmp = array[indexMin];
+        array[indexMin] = array[i];
         array[i] = tmp
     }
 
-    return array
+    return array;
 }
 
 
@@ -1381,6 +1434,19 @@ const arrForSort2 = [0,3,2,5,6,8,1,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32]
 // }
 
 //15)
+// function bubbleSort(array){
+//     for(let i = 0; i < array.length; i++){
+//         for(let j = 0; j < array.length; j++){
+//             if(array[j + 1] < array[j]){
+//                 const tmp = array[j + 1]
+//                 array[j + 1] = array[j]
+//                 array[j] = tmp;
+//             }
+//         }
+//     }
+//     return array
+// }
+//16)
 function bubbleSort(array){
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < array.length; j++){
@@ -1391,9 +1457,8 @@ function bubbleSort(array){
             }
         }
     }
-    return array
+    return array;
 }
-
 console.log('bubbleSort(arrForSort2) ', bubbleSort(arrForSort2)) 
 
 
@@ -1743,18 +1808,41 @@ const arrForSort3 = [0,3,2,5,6,8,1,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32]
 
 // }
 //15)
+// function quickSort(array){
+//     if(array.length <= 1){
+//         return array
+//     }
+
+//     let pivotIndex = Math.floor(array.length / 2),
+//         pivot      = array[pivotIndex],
+//         greater    = [],
+//         less       = [];
+    
+//     for(let i = 0; i < array.length; i++){
+//         if(i === pivotIndex) continue
+
+//         if(array[i] < pivot){
+//             less.push(array[i])
+//         }else{
+//             greater.push(array[i])
+//         }
+//     }
+
+//     return [...quickSort(less), pivot, ...quickSort(greater)]
+// }
+//16)
 function quickSort(array){
     if(array.length <= 1){
-        return array
+        return array;
     }
 
     let pivotIndex = Math.floor(array.length / 2),
         pivot      = array[pivotIndex],
-        greater    = [],
-        less       = [];
-    
+        less       = [],
+        greater    = [];
+
     for(let i = 0; i < array.length; i++){
-        if(i === pivotIndex) continue
+        if(i === pivotIndex) continue;
 
         if(array[i] < pivot){
             less.push(array[i])
@@ -2019,17 +2107,45 @@ console.log('quickSort(arrForSort3) ', quickSort(arrForSort3))
 //     return arr
 // }
 //10)
+// const heapsort = arr => {
+//     let length = arr.length;
+
+//     const heapfiy = (arr, i) => {
+//         const left = 2 * i + 1,
+//               right = 2 * i + 2;
+//         let max = i;
+//         if(left < length && arr[left] > arr[max]) max = left;
+//         if(right < length && arr[left] > arr[max]) max = right;
+//         if(max != i){
+//             [arr[max], arr[i]] = [arr[i], arr[max]];
+//             heapfiy(arr, max);
+//         }
+//     }
+
+//     for(let i = Math.floor(length / 2); i >= 0; i--){
+//         heapfiy(arr, i);
+//     }
+
+//     for(let i = length - 1; i > 0; i--){
+//         [arr[0], arr[i]] = [arr[i], arr[0]]
+//         length--;
+//         heapfiy(arr, 0)
+//     }
+
+//     return arr
+// }
+//11)
 const heapsort = arr => {
     let length = arr.length;
 
-    const heapfiy = (arr, i) => {
-        const left = 2 * i + 1,
+    const heapfiy = (array, i) => {
+        const left  = 2 * i + 1,
               right = 2 * i + 2;
         let max = i;
         if(left < length && arr[left] > arr[max]) max = left;
-        if(right < length && arr[left] > arr[max]) max = right;
+        if(right < length && arr[right] > arr[max]) max = right;
         if(max != i){
-            [arr[max], arr[i]] = [arr[i], arr[max]];
+            [arr[max], arr[i]] = [arr[i], arr[max]]
             heapfiy(arr, max);
         }
     }
@@ -2372,6 +2488,28 @@ graph.f = ['g']
 //     return false
 // }
 //15)
+// function breadthSearch(graph, start, end){
+//     let queue = [];
+
+//     queue.push(start)
+
+//     while(queue.length > 0){
+//         const current = queue.shift();
+
+//         if(!graph[current]){
+//             graph[current] = []
+//         }
+
+//         if(graph[current].includes(end)){
+//             return true;
+//         }else{
+//             queue = [...queue, ...graph[current]]
+//         }
+//     }
+
+//     return false;
+// }
+//16)
 function breadthSearch(graph, start, end){
     let queue = [];
 
@@ -2721,8 +2859,23 @@ console.log(recursive(tree))
 //     }
 // }
 //15)
+// function cashFunction(fn){
+//     const cash = {};
+
+//     return function(n){
+//         if(cash[n]){
+//             console.log('from cash', cash[n])
+//             return cash[n]
+//         }
+
+//         let result = fn(n)
+//         console.log('calc fn = ', result)
+//         cash[n] = result;
+//         return result;
+//     }
+// }
 function cashFunction(fn){
-    const cash = {};
+    const cash = {}
 
     return function(n){
         if(cash[n]){
